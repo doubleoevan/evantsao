35efff6e0a2340c96a6d18b8630758deaed4e18f
doubleoevan <doubleoevan@gmail.com>
2026-03-04 17:23:49 -0800

    homepage text and page layouts

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -5,7 +5,7 @@ export type MenuItem = {
 
 export const MENU_ITEMS: MenuItem[] = [
   { href: "/", label: "Home" },
-  // { href: "/writing", label: "Writing" },
+  { href: "/writing", label: "Writing" },
   { href: "/music", label: "Music" },
   { href: "/improv", label: "Improv" },
   { href: "/technology", label: "Technology" },
